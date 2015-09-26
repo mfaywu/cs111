203afa163cb47899160bde099b819f676c244560
M. Fay Wu <wu.mfay@gmail.com>
2015-09-26 12:05:52 -0700

    Added stack for commands

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -8,8 +8,53 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+struct stack_node
+{
+	command_t command;
+	struct stack_node *next;
+}
+
+void init(struct stack_node* head)
+{
+    head = NULL;
+}
+
+struct stack_node* push(struct stack_node* head, command_t command)
+{
+    struct stack_node* temp = (struct stack_node*)malloc(sizeof(struct stack_node));
+    if(temp == NULL)
+    {
+        exit(0);
+    }
+    temp->command = command;
+    temp->next = head;
+    head = temp;
+    return head;
+}
+
+struct stack_node* pop(struct stack_node *head, command_t *command)
+{
+    struct stack_node* temp = head;
+    *command = head->command;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+command_t peek(struct stack_node* head)
+{
+	return head->command;
+}
+
+
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
+struct command_stream //TODO
+{
+	command_t *command;
+	struct command_stream *next;
+}
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -18,8 +63,17 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+  //error (1, 0, "command reading not yet implemented");
+  //return 0;
+
+	char curr;
+
+	while((curr = get_next_byte(get_next_byte_argument)) != EOF)
+	{
+
+	}
+
+	//return some command_stream;
 }
 
 command_t
