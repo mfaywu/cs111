4e96a63ab6eb3c65e003dcbf95427af66f7414e4
M. Fay Wu <wu.mfay@gmail.com>
2015-09-28 13:39:51 -0700

    Fixed pointer issues

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -9,17 +9,22 @@
    static function definitions, etc.  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-int numberOfOper(cstring * operator)
+int numberOfOper(char *operator)
 {
-	switch(operator)
+	switch(operator[0])
 	{
 		case '(': return 0;
 		case ')': return 1;
 		case ';': return 2;
-		case '|': return 3;
-		case '&&': return 4;
-		case '||': return 5;
+		case '|': 
+			if(operator[1] == '|')
+				return 3;
+			else
+				return 5;
+		case '&': return 4;
 		case '<': return 6;
 		default: return 7; //Aka '>'
 	}
@@ -65,10 +70,12 @@ struct stack_node* push(struct stack_node* head, int operator, command_t command
     struct stack_node* temp = (struct stack_node*)malloc(sizeof(struct stack_node));
     if(temp == NULL)
     {
-        exit(0);
+        error(1, 0, "No space left.");
     }
+	if(operator != -1)
 		temp->operator = operator;
-    temp->command = command;
+	if(command != NULL)
+		temp->command = command;
     temp->next = head;
     head = temp;
     return head;
@@ -97,60 +104,66 @@ int peekOperator(struct stack_node* head)
 //TODO: Returns true if curr is separate from the currCommand
 //false if curr should be concatenated to currCommand
 //isNewCommand returns true if this is the end of the complete command
-bool isNewElement(char curr, cstring* currElement, bool *isNewCommand)
+bool isNewElement(char curr, char *currElement, bool *isNewCommand)
 {
-
+	return false; //TODO
 }
 
-bool makeElement(char curr, cstring* currElement)
+bool makeElement(char curr, char *currElement)
 {
-
+	return false; //TODO
 }
 
-command combine (command firstCommand, command secondCommand, int temOper) //TODO
+command_t makeCommand(char *currElement) 
 {
-
+	return NULL; //TODO
 }
 
-void process(cstring *currElement, struct stack_node *operatorStack, struct stack_node *commandStack)
+command_t combine (command_t firstCommand, command_t secondCommand, int temOper) //TODO
 {
-	int opNum = numberOfOper(*currElement);
+	return NULL;
+}
+
+void process(char *currElement, struct stack_node *operatorStack, struct stack_node *commandStack)
+{
+	int opNum = numberOfOper(currElement);
 
 	if(opNum == -1)
 	{
-		push(*commandStack, NULL, *currElement);
+		command_t tempCommand = makeCommand(currElement);
+		push(commandStack, -1, tempCommand);
 	}
 	else
 	{
-		if(*operatorstack == NULL)
+		if(operatorStack == NULL)
 		{
-			push(*operatorStack, *currElement, NULL);
+			push(operatorStack, *currElement, NULL);
 		}
 		else
 		{
-			if(precedence(opNum)) > precedence(peekOperator(*operatorStack)))
+			if(precedence(opNum) > precedence(peekOperator(operatorStack)))
 			{
-				push(*operatorStack, *currElement, NULL);
+				push(operatorStack, *currElement, NULL);
 			}
 			else
 			{
-				while(numberOfOper(peekOperator(*operatorStack)) != 0 && precedence(opNum) <= precedence(peekOperator(*operatorStack))) //TODO: #define '(' as 0
+				while(precedence(peekOperator(operatorStack)) != 0 && precedence(opNum) <= precedence(peekOperator(operatorStack))) //TODO: #define '(' as 0
 				{
-					int tempOper = peekOperator(*operatorStack);
-					pop(*operatorStack);
-					int secondCommand = peekCommand(*commandStack);
-					pop(*commandStack);
-					int firstCommand = peekCommand(*commandStack);
-					pop(*commandStack);
-					command newCommand = combine(firstCommand, secondCommand, tempOper); //TODO: Write combine
-					push(*commandStack, NULL, newCommand);
-					if(*operatorStack == NULL)
+					int tempOper = peekOperator(operatorStack);
+					pop(operatorStack);
+					command_t secondCommand = peekCommand(commandStack);
+					pop(commandStack);
+					command_t firstCommand = peekCommand(commandStack);
+					pop(commandStack);
+					command_t newCommand = combine(firstCommand, secondCommand, tempOper); //TODO: Write combine
+					push(commandStack, -1, newCommand);
+					if(operatorStack == NULL)
 					{
 						break;
 					}
 				}
 			}
-			push(*operatorStack, *currElement, NULL);
+			push(operatorStack, *currElement, NULL);
 		}
 	}
 }
@@ -164,18 +177,15 @@ struct command_stream //TODO
 	struct command_stream *next;
 }
 
-command_stream_t
-make_command_stream (int (*get_next_byte) (void *),
+command_stream_t make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  //error (1, 0, "command reading not yet implemented");
-  //return 0;
 
 	char curr;
-	cstring currElement = NULL;
+	char *currElement = NULL;
 	bool *isNewCommand;
 	//have a pointer to the first complete command, which we'll call stream
 
@@ -199,7 +209,7 @@ make_command_stream (int (*get_next_byte) (void *),
 			makeElement(curr, *currElement);
 		}
 	}
-
+	return NULL;
 	//return stream, which is some pointer to a linkedlist of commands aka command_stream;
 }
 
